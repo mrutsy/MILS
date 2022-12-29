bb213d225e88a39f751fddf2009db3df1ab82312
mrutsy <roman@zhilk.in>
2022-12-29 22:26:48 +0700

    Добавил composer и первую библиотеку для работы с API Telegram.

---
+++ libraries/composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1a7c7e9e4fc1bf19a432c943d60db37e9ef8d392',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1a7c7e9e4fc1bf19a432c943d60db37e9ef8d392',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'telegram-bot/api' => array(
+            'pretty_version' => 'v2.3.24',
+            'version' => '2.3.24.0',
+            'reference' => '579f64d0ffe7f21d1f61cb8734e297686b286bf7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../telegram-bot/api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
